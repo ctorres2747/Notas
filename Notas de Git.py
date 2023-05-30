07029255651d90f8af2f8de37ec5809ab4fcc791
Cristhiant <cristhiant@hotmail.com>
2023-05-30 15:15:21 -0500

    Este es mi segundo commit

---
+++ Notas de Git.py
@@ -0,0 +1,19 @@
+#Configuración de Git
+
+'''Nos paramos en la carpeta que queremos crear el repositorio 
+#git config --global user.name "aqui va el nombre de usuario" (si ya está creado devuelve el nombre)
+#git config --global user.email "aqui va el correo del usuario'''
+
+#Para crear una carpeta nueva en la terminal
+'''git mkdir "aqui va el nombre de la carpeta'''
+
+#Funciones de Git
+'''
+git init   -> crea el repositorio en la carpeta
+git status   -> me da el status de los ficheros que están aderidos y los que no
+git add ____  -> en el espacio va el nommbre del fichero que debemos añadir al control de versiones
+git commit -m "aquí va un comentario"  -> crea una foto de las modificaciones hechas hasta ese momento se debe añadir un comentario
+git log    -> devuelve las fotos realizadas y el usuario que las ejecutó
+git checkout _____ -> en el espacio va el nombre del fichero que queremos devolver a la commit anterior
+
+'''
